7c064a72fa7169e1d8f39b593b682fd503b90df3
Michael Bishop <mbtyke@gmail.com>
2014-02-19 00:52:33 -0500

    Shortcutted setting the continuation for exceptions if there is no reason to
    Hopefully, this is a speed gain.

--- lib/mulligan/kernel.rb
+++ lib/mulligan/kernel.rb
@@ -4,6 +4,7 @@ module Mulligan
       super(*args)
     rescue Exception => e
       e.__send__(:__load_builder__, &block) unless block.nil?
+      super(e) if e.__send__(:__builder__).nil?
       callcc{|c| e.send(:__set_continuation__, c); super(e)}
     end
   end
