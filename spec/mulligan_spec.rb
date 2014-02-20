9631e0426d4fa179121fc7be98eae8c86e4c61ac
Michael Bishop <mbtyke@gmail.com>
2014-02-20 17:05:44 -0500

    Changed the terminology from 'a restart' to 'a recovery'. I wanted a word that could not both be a noun and a verb. This allows me to change `#restart_invoke` to `#recover`

--- spec/mulligan_spec.rb
+++ spec/mulligan_spec.rb
@@ -11,62 +11,62 @@ describe Mulligan do
     end
 
     it 'should correctly report missing strategies' do
-      outer_test(style){|e|e.restart_exist?(:aaa)}.should be_false
+      outer_test(style){|e|e.recovery_exist?(:aaa)}.should be_false
     end
 
     it 'should correctly report included strategies' do
-      outer_test(style){|e|e.restart_exist?(:ignore)}.should be_true
+      outer_test(style){|e|e.recovery_exist?(:ignore)}.should be_true
     end
 
     it 'should raise a control exception when invoking a non-existent restart' do
-      expect { outer_test(style){|e|e.restart_invoke :aaa} }.to raise_error(Mulligan::ControlException)
+      expect { outer_test(style){|e|e.recover :aaa} }.to raise_error(Mulligan::ControlException)
     end
 
     it 'should not raise an exception when invoking the ignore restart' do
-      expect { outer_test(style){|e|e.restart_invoke :ignore} }.to_not raise_exception
+      expect { outer_test(style){|e|e.recover :ignore} }.to_not raise_exception
     end
 
     it 'should return the parameter sent when invoking the return_param restart' do
-      result = outer_test(style){|e|e.restart_invoke(:return_param, 5)}
+      result = outer_test(style){|e|e.recover(:return_param, 5)}
       result.should be(5)
     end
 
     context "and follows the continutation to the correct raise" do
       it 'should return the parameter sent when invoking the return_param restart' do
-        result = outer_test(style){|e|e.restart_invoke(:return_param2, 5)}
+        result = outer_test(style){|e|e.recover(:return_param2, 5)}
         result.should be(25)
       end
     end
     
     it "should ignore setting a restart when passed no block" do
-      expect { outer_test(style){|e|e.restart_invoke :no_block} }.to raise_error(Mulligan::ControlException)
+      expect { outer_test(style){|e|e.recover :no_block} }.to raise_error(Mulligan::ControlException)
     end
     
     describe "restart options" do
       it "should not return the block" do
         data = nil
         outer_test(style) do |e|
-          data = e.restart_options(:return_param)[:block]
-          e.restart_invoke(:return_param)
+          data = e.recovery_data(:return_param)[:block]
+          e.recover(:return_param)
         end
         expect(data).to be_nil
       end
 
-      it "should pass data created in set_restart" do
+      it "should pass data created in set_recovery" do
         data = nil
         outer_test(style) do |e|
-          data = e.restart_options(:return_param)[:data]
-          e.restart_invoke(:return_param)
+          data = e.recovery_data(:return_param)[:data]
+          e.recover(:return_param)
         end
         expect(data).to be(5)
       end
 
       it "should be read-only" do
         result = outer_test(style) do |e|
-          e.restart_options(:return_param)[:new_entry] = 5
-          e.restart_invoke(:return_param, e)
+          e.recovery_data(:return_param)[:new_entry] = 5
+          e.recover(:return_param, e)
         end
-        expect(result.restart_options(:return_param)[:new_entry]).to be_nil
+        expect(result.recovery_data(:return_param)[:new_entry]).to be_nil
       end
     end
     
@@ -79,8 +79,8 @@ describe Mulligan do
 
     it "shouldn't fail when calling a restart before raising" do
       t = Exception.new("Test Exception")
-      t.set_restart(:ignore) {|p|}
-      expect{t.restart_invoke(:ignore)}.to_not raise_error
+      t.set_recovery(:ignore) {|p|}
+      expect{t.recover(:ignore)}.to_not raise_error
     end
   end
 
@@ -90,13 +90,13 @@ describe Mulligan do
 
     it "should propgate restarts when raising a pre-existing exception" do
       t = Exception.new("Test Exception")
-      t.set_restart(:ignore) {|p|}
+      t.set_recovery(:ignore) {|p|}
       begin
         raise t do |e|
-          e.set_restart(:return_param){|p|p}
+          e.set_recovery(:return_param){|p|p}
         end
       rescue Exception => e
-        expect(e.restart_exist?:ignore).to be_true
+        expect(e.recovery_exist?:ignore).to be_true
       end
     end
   end
@@ -106,10 +106,10 @@ def core_test(style)
   t =  "Test Exception"
   t = Exception.new("Test Exception") if style == :manual
   raise t do |e|
-    e.set_restart(:ignore){|p|p}
-    e.set_restart(:no_block)
-    e.set_restart(:return_param, data: 5){|p|p}
-    e.set_restart(:return_param2){|p|p}
+    e.set_recovery(:ignore){|p|p}
+    e.set_recovery(:no_block)
+    e.set_recovery(:return_param, data: 5){|p|p}
+    e.set_recovery(:return_param2){|p|p}
   end
 end
 
@@ -118,10 +118,10 @@ def inner_test(style = :manual)
   rescue Exception => e
     should_retry = false
     result = raise(e) do |e|
-      e.set_restart(:retry){should_retry = true}
+      e.set_recovery(:retry){should_retry = true}
       # here we add 10 so we can ensue we are in fact, overriding
       # the behavior for the same restart as defined in core_test
-      e.set_restart(:return_param2){|p|p+10}
+      e.set_recovery(:return_param2){|p|p+10}
     end
     retry if should_retry
     # here we add 10 so we can differentiate retries in `inner_test` from `core_test`
