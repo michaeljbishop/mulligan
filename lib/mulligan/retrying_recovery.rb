168d24417c46f0b75fb08d97717e9e538c1305ba
Michael Bishop <mbtyke@gmail.com>
2014-03-21 00:09:00 -0400

    Added metadata to IgnoringRecovery. Added a standard RetryingRecovery class.

---
+++ lib/mulligan/retrying_recovery.rb
@@ -0,0 +1,22 @@
+module Mulligan
+  class RetryingRecovery < Recovery
+    self.summary = <<END
+Performs again the last task which caused the failure.
+END
+    self.discussion = <<END
+Attributes:
+  'count' - The number of times this recovery has been invoked. In this way, you can keep track of how many times the code has been retried and perhaps limit the total number of retries.
+END
+    attr_reader :count
+
+    def initialize
+      @count = 0
+    end
+
+    def bump
+      @count = @count + 1
+    end
+    
+  end
+end
+
